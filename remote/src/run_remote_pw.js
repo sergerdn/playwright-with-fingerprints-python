1aa08921b85f532e990b6db63924ca552445895a
sergerdn <64213648+sergerdn@users.noreply.github.com>
2023-09-30 00:39:08 -0700

    chore: bump versions

--- remote/src/run_remote_pw.js
+++ remote/src/run_remote_pw.js
@@ -3,18 +3,18 @@ const { plugin } = require("playwright-with-fingerprints")
 
 const os = require("os")
 const fs = require("fs").promises
-const { chromium } = require("playwright")
+//const { chromium } = require("playwright")
 const { randomUUID } = require("crypto")
 const path = require("path")
 
-async function runContext(fingerprintKey, userDataDir, profileName, port) {
+async function runContext(fingerprintKey, userDataDir, port) {
   console.log(
     `Running with:
        fingerprintKey: ${fingerprintKey}, 
        userDataDir: ${userDataDir},
-       profileName: ${profileName},
        port: ${port}`
   )
+
   const fingerprint = await plugin.fetch(fingerprintKey, {
     tags: ["Microsoft Windows", "Chrome"],
     minBrowserVersion: 112,
@@ -23,9 +23,9 @@ async function runContext(fingerprintKey, userDataDir, profileName, port) {
     maxWidth: 1920,
     maxHeight: 1080
   })
+
   // Save the fingerprint to a file
   await fs.writeFile(path.join(userDataDir, "fingerprint.json"), JSON.stringify(fingerprint))
-  await fs.writeFile(path.join(userDataDir, "fingerprint_1.json"), fingerprint)
 
   plugin.useFingerprint(fingerprint)
   // https://peter.sh/experiments/chromium-command-line-switches/
@@ -33,20 +33,27 @@ async function runContext(fingerprintKey, userDataDir, profileName, port) {
   // Launch the browser instance:
   const browserContext = await plugin.launchPersistentContext(userDataDir, {
     headless: false,
-    args: [`--remote-debugging-port=${port}`, `--profile-directory=${profileName}`]
+    logger: {
+      isEnabled: (name, severity) => true,
+      log: (name, severity, message, args) => console.log(`${name} ${message}`)
+    },
+    args: [`--remote-debugging-port=${port}`]
   })
 
-  console.log("Browser headless launched")
+  console.log(`Browser launched and can be controlled remotely via http://localhost:${port}`)
   //await browserContext.close()
 }
 
 ;(async () => {
   const fingerprintKey = process.env.FINGERPRINT_KEY
   if (fingerprintKey === undefined) {
-    console.log("Please set the FINGERPRINT_KEY environment variable")
+    console.log("Please set the FINGERPRINT_KEY environment variable.")
     process.exit(1)
   }
+
   const userDataDir = await fs.mkdtemp(path.join(os.tmpdir(), "chrome-profile-"))
-  const profileName = randomUUID()
-  await runContext(fingerprintKey, userDataDir, profileName, 9222)
+  const profileName = `profile-${randomUUID()}`
+  console.log(`Using profile name: ${profileName}`)
+
+  await runContext(fingerprintKey, userDataDir, 9222)
 })()
