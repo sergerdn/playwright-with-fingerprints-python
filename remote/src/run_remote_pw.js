62c3b5f3ccda6a47a91e309674a20b5170ddc729
sergerdn <64213648+sergerdn@users.noreply.github.com>
2023-10-01 02:19:18 -0700

    chore(javascript): refactoring to use setWorkingFolder instead of env variable

--- remote/src/run_remote_pw.js
+++ remote/src/run_remote_pw.js
@@ -4,20 +4,24 @@ const { plugin } = require("playwright-with-fingerprints")
 const os = require("os")
 const fs = require("fs").promises
 //const { chromium } = require("playwright")
+const { FingerprintOptions } = require("browser-with-fingerprints")
 const { randomUUID } = require("crypto")
 const path = require("path")
 
-async function runContext(fingerprintKey, userDataDir, port) {
+async function runContext(fingerprintKey, workingFolder, userDataDir, port) {
   console.log(
     `Running with:
        fingerprintKey: ${fingerprintKey}, 
+       workingFolder: ${workingFolder},
        userDataDir: ${userDataDir},
        port: ${port}`
   )
 
+  plugin.setWorkingFolder(workingFolder)
+
   const fingerprint = await plugin.fetch(fingerprintKey, {
     tags: ["Microsoft Windows", "Chrome"],
-    minBrowserVersion: 112,
+    minBrowserVersion: 117,
     minWidth: 1366,
     minHeight: 768,
     maxWidth: 1920,
@@ -27,7 +31,13 @@ async function runContext(fingerprintKey, userDataDir, port) {
   // Save the fingerprint to a file
   await fs.writeFile(path.join(userDataDir, "fingerprint.json"), JSON.stringify(fingerprint))
 
-  plugin.useFingerprint(fingerprint)
+  const fingerprintOptions = {
+    emulateDeviceScaleFactor: false,
+    usePerfectCanvas: true,
+    useFontPack: true
+  }
+
+  plugin.useFingerprint(fingerprint, fingerprintOptions)
   // https://peter.sh/experiments/chromium-command-line-switches/
 
   // Launch the browser instance:
@@ -46,14 +56,22 @@ async function runContext(fingerprintKey, userDataDir, port) {
 
 ;(async () => {
   const fingerprintKey = process.env.FINGERPRINT_KEY
-  if (fingerprintKey === undefined) {
+  if (!fingerprintKey) {
     console.log("Please set the FINGERPRINT_KEY environment variable.")
     process.exit(1)
   }
+
+  let workingFolder = process.env.WORKING_FOLDER
+  if (!workingFolder) {
+    console.log("Please set the WORKING_FOLDER environment variable.")
+    process.exit(1)
+  }
+
+  workingFolder = path.resolve(workingFolder)
 
   const userDataDir = await fs.mkdtemp(path.join(os.tmpdir(), "chrome-profile-"))
   const profileName = `profile-${randomUUID()}`
   console.log(`Using profile name: ${profileName}`)
 
-  await runContext(fingerprintKey, userDataDir, 9222)
+  await runContext(fingerprintKey, workingFolder, userDataDir, 9222)
 })()
